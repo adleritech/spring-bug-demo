467cb310c4473ccf908b5a6740092ca264805b5b
Lukas Krecan <lukas.krecan@adleritech.net>
2018-06-05 14:47:32 +0200

    Reproduce Spring Data Bug

---
+++ src/main/java/com/example/demo/Item.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document
+public class Item {
+
+    @Id
+    private String id;
+
+    @CreatedDate
+    private Date createdAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
